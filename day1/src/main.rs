fd7961ca5d943b32adf052c0f9a009d348f8177a
oscarmartensson <oscar.mattias.martensson@hotmail.com>
2021-12-02 09:09:03 +0100

    Update with the 2nd part of day1.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,10 +4,12 @@ use std::path::Path;
 
 fn main() {
     let file_name = "input.txt";
-    // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines(file_name) {
-        // Consumes the iterator, returns an (Optional) String
 
+    // Read file.
+    if let Ok(lines) = read_lines(file_name) {
+
+        // --- Part 1 ---
+        // Consumes the iterator, returns an (Optional) String
         let mut counter = 0u16;
         let mut previous_value:i64 = 0xDEADBEEF;
         let mut current_value:i64;
@@ -31,6 +33,44 @@ fn main() {
 
         println!("Number of times values decreased: {}", counter);
     }
+
+    // Read the input again, easiest this way even though ugly.
+    if let Ok(lines) = read_lines(file_name) {
+
+        // --- Part 2 ---
+        let mut counter = 0u16;
+        let mut current_sum: i64;
+        let mut previous_sum: i64 = 0xDEADBEEFi64;
+
+        let mut moving_window: [i64; 3] = [0, 0, 0];
+        let v = Vec::from_iter(lines);
+
+        for i in 0..v.len() {
+            if i + 2 < v.len()
+            {
+                moving_window[0] = v[i].as_ref().unwrap().parse::<i64>().unwrap();
+                moving_window[1] = v[i+1].as_ref().unwrap().parse::<i64>().unwrap();
+                moving_window[2] = v[i+2].as_ref().unwrap().parse::<i64>().unwrap();
+
+                current_sum = moving_window[0] + moving_window[1] + moving_window[2];
+
+                if previous_sum == 0xDEADBEEF {
+                    // First iteration.
+                    previous_sum = current_sum;
+                    continue;
+                }
+
+                if current_sum > previous_sum {
+                    counter += 1;
+                }
+
+                previous_sum = current_sum;
+            }
+        }
+
+        println!("Number of times moving window sum increased: {}", counter);
+    }
+
 }
 
 // The output is wrapped in a Result to allow matching on errors
