ec2e01b0250198b53c0c4ac4ae9f9f48ab8f1c00
oscarmartensson <oscar.mattias.martensson@hotmail.com>
2021-12-02 20:44:14 +0100

    Second part of day2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,8 +10,6 @@ fn first_part(input_path: &str) {
     // Read file.
     if let Ok(lines) = read_lines(input_path) {
 
-        // --- Part 1 ---
-        // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
 
@@ -33,8 +31,44 @@ fn first_part(input_path: &str) {
     }
 }
 
+fn second_part(input_path: &str) {
+
+    let mut horizontal_distance: i64 = 0;
+    let mut vertical_distance: i64 = 0;
+    let mut aim = 0;
+
+    // Read file.
+    if let Ok(lines) = read_lines(input_path) {
+
+        for line in lines {
+            if let Ok(ip) = line {
+
+                let split_line = ip.split(' ').collect::<Vec<&str>>();
+                let direction = split_line[0];
+                let distance = split_line[1].parse::<i64>().unwrap() as i64;
+
+                match direction {
+                    "forward" => {
+                        horizontal_distance += distance;
+                        vertical_distance += aim * distance;
+                    }
+                    "up" => { aim -= distance; }
+                    "down" => { aim += distance; }
+                    _ => {/*All other cases, must be covered due to compilator E0004 complaint.*/}
+                }
+            }
+        }
+
+        println!("Final horizontal value: {}", horizontal_distance);
+        println!("Final vertical value: {}", vertical_distance);
+        println!("Final aim value: {}", aim);
+        println!("Multiplied by each other: {}", vertical_distance * horizontal_distance);
+    }
+}
+
 fn main() {
     first_part("input.txt");
+    second_part("input.txt");
 }
 
 // The output is wrapped in a Result to allow matching on errors
